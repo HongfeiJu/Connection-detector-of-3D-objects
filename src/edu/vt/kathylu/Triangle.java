b042987ab4110a64d4dcb4c98154b0cbb1eb493f
HongfeiJu <hongfeiju@gmail.com>
2020-04-04 19:19:09 +0800

    create vertex and triangle class

---
+++ src/edu/vt/kathylu/Triangle.java
@@ -0,0 +1,32 @@
+package edu.vt.kathylu;
+
+public class Triangle {
+    private Vertex v1, v2, v3;
+
+    public Triangle(){
+    }
+
+    public Vertex getV1() {
+        return v1;
+    }
+
+    public void setV1(Vertex v1) {
+        this.v1 = v1;
+    }
+
+    public Vertex getV2() {
+        return v2;
+    }
+
+    public void setV2(Vertex v2) {
+        this.v2 = v2;
+    }
+
+    public Vertex getV3() {
+        return v3;
+    }
+
+    public void setV3(Vertex v3) {
+        this.v3 = v3;
+    }
+}
---
+++ src/edu/vt/kathylu/Vertex.java
@@ -0,0 +1,33 @@
+package edu.vt.kathylu;
+
+public class Vertex {
+    private double x = 0, y = 0, z = 0;
+
+    public Vertex(){
+
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+}
